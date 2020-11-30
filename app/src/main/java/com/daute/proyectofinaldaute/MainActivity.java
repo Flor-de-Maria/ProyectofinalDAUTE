24fefecffaaac32017a74ae2c58c03b0208fe1b1
CarlosEstrada141218 <64821914+CarlosEstrada141218@users.noreply.github.com>
2020-11-29 18:58:06 -0600

    diseño terminado 1.1

--- app/src/main/java/com/daute/proyectofinaldaute/MainActivity.java
+++ app/src/main/java/com/daute/proyectofinaldaute/MainActivity.java
@@ -3,22 +3,15 @@ package com.daute.proyectofinaldaute;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
 
-import com.daute.proyectofinaldaute.ui.dashboard.About;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     @Override
